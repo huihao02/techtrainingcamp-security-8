9c2cbf1fd46adef795e2f3d921960609a73d4287
pyyyyp <78945593+pyyyyp@users.noreply.github.com>
2021-11-12 19:57:51 +0800

    注册相关

--- utils/constants.go
+++ utils/constants.go
@@ -12,3 +12,10 @@ var BANNED_TIME time.Duration = 5 * time.Minute
 
 //session过期时间
 var EXPIRE_TIME time.Duration = 3 * time.Hour
+
+
+//一个手机号的最大注册次数
+var LIMIT_REGISTER_TIMES = 10
+
+//一个手机号注册后多久才能重新注册
+var PHONENUM_REGISTER_INTERVAL = 24 * time.Hour
